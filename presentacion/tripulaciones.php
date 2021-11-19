1ba6b35d048ab45ec2a4db6d14a56149accb04d8
JuanGJ <juangj1b@gmail.com>
2021-11-19 12:50:53 +0100

    Cambios CSS

--- presentacion/tripulaciones.php
+++ presentacion/tripulaciones.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="styles/gestor.css">
+    <link rel="stylesheet" href="styles/tripulacion.css">
     <?php include_once "../negocio/procesarTripulacion.php.php"?>
     <title>Vuelos</title>
 </head>
