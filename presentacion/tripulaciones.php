ad5e6aceee5fdfaf721c6e0ef0f7a06cc9aa3705
jesus-2808 <jesus.gonzalez.munoz.al@iespoligonosur.org>
2021-11-19 13:32:47 +0100

    lo ultimismo

--- presentacion/tripulaciones.php
+++ presentacion/tripulaciones.php
@@ -1,1 +1,19 @@
-<!-- vuelos de su compañia y editarlos (¿borrar?)-->
+<?php
+include "databaseManagement.inc.php";
+
+    function listaVuelosTripulacion(){
+        if(isset($_SESSION["correspondencia"])){
+            if($_SESSION["correspondencia"]!="gestor"){
+                $operadora=$_GET["operadora"];
+                $listaVuelos=consultaVuelosCompanya($operadora);
+
+                if($listaVuelos>0){
+                    return $listaVuelos;
+                }
+            }else{
+                header("location: index.php");
+            }
+        }else{
+            header("location: index.php");
+        }
+    }
