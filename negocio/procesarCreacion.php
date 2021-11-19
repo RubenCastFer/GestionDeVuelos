4368f78817ead0878d6876139e4bfc4b7ceb6ec3
KimiaProg <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-11-19 12:24:16 +0100

    creacion

--- negocio/procesarCreacion.php
+++ negocio/procesarCreacion.php
@@ -2,4 +2,24 @@
     /*
         recoger datos de gestor segun la sesiones, gestinar con bd y devolver resultados "seguro"
     */ 
+
+    $error = "";
+    if (count($_POST) > 0) {
+        function seguro($valor)
+        {
+            $valor = strip_tags($valor);
+            $valor = stripslashes($valor);
+            $valor = htmlspecialchars($valor);
+            return $valor;
+        }
+        
+        $id = VuelosInsertar(seguro($_POST["origen"]), $_POST["destino"], $_POST["operadora"], $_POST["fecha"], $_POST["cantidadViajero"]);
+        if ($id != 0) {
+            header("Location: ../presentacion/gestor.php");
+            exit();
+        } else {
+            $error = "Datos incorrectos";
+        }
+    }
+
 ?>
