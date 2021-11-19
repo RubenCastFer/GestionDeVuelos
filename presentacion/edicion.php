0d389d18ef082ea4012f4420ae793aa89151d563
javierDiazDaw <javier.diaz.roman.al@iespoligonosur.org>
2021-11-19 12:26:35 +0100

    creacion

--- presentacion/edicion.php
+++ presentacion/edicion.php
@@ -9,16 +9,26 @@
     <title>Edicion</title>
 </head>
 <body>
+
+    <!--<?php
+
+        //variables de edicion
+
+
+        
+
+    ?>-->
+
     <header>
         <a id="titulocentral" href="#"> <!-- Enlace, el href puede cambiar según la sesión-->
             <span class="titulo">Gestor de viajes</span>
             <span class="subtitulo">Desarrollo Web en Entorno Servidor</span>
-        </a> 
-    
+        </a>    
         <nav>
             <ul>
                 <!-- Aquí habrá un if, dependiendo de la sesión. Si es gestor, saldrá la opción de crear-->
-                <li><a href="login.php">Iniciar sesión</a></li>
+                <li><a href="login.php">Sesión iniciada</a></li>
+                <li><a href="gestor.php">Cerrar sesión</a></li>
             </ul>
         </nav>
 
@@ -41,17 +51,10 @@
                 <br>
                 <label for="name">Cantidad de viajeros</label>
                 <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Cantidad de viajeros">
-                <input class="botons" type="submit" value="Entrar">
-                <button class="button login__submit">
-                    <span class="button__text"><a href="negocio/procesarLogin.php">Inicia sesión</a></span>
-                    <i class="button__icon"></i>
-                </button>
-                
-              </section>
-            <br><br><br><br><br><br>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12685.635060397448!2d-5.9817521!3d37.3565037!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5c813182e6db9555!2sEstadio%20Benito%20Villamar%C3%ADn!5e0!3m2!1ses!2ses!4v1635502561261!5m2!1ses!2ses" width="600" height="450" style="border:0; border-radius: 20px; width: 60%;" allowfullscreen="" loading="lazy"></iframe>
-            <br><br><br><br><br><br>
-            </div>
+                <br><br>
+                <button class="button__text"><a href="negocio/procesarLogin.php">Entrar</a></buttom>  
+            </section>
+            <br><br>
     
     </header>
     <div id="contenedor">
