6d4e4793cb1b498b99e91cfea52c0fc981626dc8
KimiaProg <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-11-19 11:59:36 +0100

    login-invitado

--- negocio/procesarImvitado.php
+++ negocio/procesarImvitado.php
@@ -1,5 +1,37 @@
 <?php
-    /*
+/*
         recoger datos de invitados segun el filtro, gestinar con bd y devolver [] "seguro"
-    */ 
-?>
+    */
+include_once "databaseManagement.inc.php";
+
+session_start();
+
+function seguro($valor)
+{
+    $valor = strip_tags($valor);
+    $valor = stripslashes($valor);
+    $valor = htmlspecialchars($valor);
+    return $valor;
+}
+function sesionComprobar()
+{
+    if (isset($_SESSION["aeroOrigen"]) && isset($_SESSION["aeroDestino"])) {
+        $listaVuelos = obtenerVuelos();
+    } elseif (isset($_SESSION["aeroOrigen"])) {
+        $listaVuelos = consultaVuelosInvitadosLlegada(seguro($_POST["ciudad"]));
+    } elseif (isset($_SESSION["aeroDestino"])) {
+        $listaVuelos = consultaVuelosInvitadosSalida(seguro($_POST["ciudad"]));
+    }
+    return $listaVuelos;
+}
+if (count($_POST) > 0) {
+    if (isset($_POST["aero"]["origen"])) {
+        $_SESSION["aeroOrigen"] = 1;
+    }
+    if (isset($_POST["aero"]["destino"])) {
+        $_SESSION["aeroDestino"] = 1;
+    }
+    sesionComprobar();
+} else {
+    sesionComprobar();
+}
--- negocio/procesarLogin.php
+++ negocio/procesarLogin.php
@@ -2,4 +2,25 @@
     /*
         buscar usuario redireccionar a gestor o a tripulacion por sesion
     */ 
-?>
+
+    $error='';
+    if (count($_POST) > 0) {
+        session_start();
+        $nombre = $_POST["nombre"];
+        $pass = $_POST["pass"];
+        $datosUsu=obtenerUsuario($nombre);
+        if($datosUsu!=''){
+            if(password_verify($pass,$datosUsu["contrasenia"])){
+                if($datosUsu["correspondencia"]=="gestor"){
+                    $_SESSION["correspondencia"]="gestor";
+                    header("Location: ../presentacion/gestor.php");
+                }else{
+                    header("Location: ../presentacion/tripulaciones.php?operadora=".$datosUsu["correspondencia"]);
+                }
+            }else{
+                $error="Contraseña incorrecta";
+            }
+        }else{
+            $error="Usuario no existe";
+        }
+    }
