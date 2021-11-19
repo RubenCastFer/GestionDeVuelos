5f13b7d81b5428e3f2c14e3dc7eb7c6fb5da2990
jesus-2808 <jesus.gonzalez.munoz.al@iespoligonosur.org>
2021-11-19 11:40:33 +0100

    prueba

--- persistencia/databaseManagement.inc.php
+++ persistencia/databaseManagement.inc.php
@@ -1,27 +1,77 @@
 <?php
 $servidor="localhost";
-$baseDatos=""; 
-$usuario= "developer";
-$pass="developer";
+$baseDatos="aeropuertos"; 
+$usuario= "root";
+$pass="root";
 
 //funciones base de datos:
     //usuario invitado
-    function consultaVuelosInvitadosLlegada(){
-        //hola
+    function consultaVuelosInvitadosLlegada($destino){
+       try {
+        $conexion = new PDO("mysql:host=" . $GLOBALS['servidor'] . ";dbname=" . $GLOBALS['baseDatos'], $GLOBALS['usuario'], $GLOBALS['pass']);
+        $sql=$conexion->prepare("SELECT * FROM vuelos WHERE destino=:destino");
+        $sql->bindParam(":destino", $id);
+        $sql->execute();
+        $arrayVuelos=[];
+        while ($fila = $sql->fetch(PDO::FETCH_ASSOC)) {
+            $arrayVuelos[]=$fila;
+        }
+        $conexion=null;
+        return $arrayVuelos;
+ 
+       } catch (PDOException $e) {
+        echo $e;
+       }
+   
     }
 
-    function consultaVuelosInvitadosSalida(){
-
+    function consultaVuelosInvitadosSalida($origen){
+        try {
+          $conexion = new PDO("mysql:host=" . $GLOBALS['servidor'] . ";dbname=" . $GLOBALS['baseDatos'], $GLOBALS['usuario'], $GLOBALS['pass']);
+          $sql=$conexion->prepare("SELECT * FROM vuelos WHERE origen =:origen");
+          $sql->bindParam(":origen", $origen);
+          $sql->execute();
+          $arrayVuelos=[];
+            while ($fila = $sql->fetch(PDO::FETCH_ASSOC)) {
+                $arrayVuelos[]=$fila;
+            }
+          
+          $conexion=null;
+          return $fila;
+        } catch (PDOException $e) {
+           echo $e;
+        }
+       
     }
 
 
     //usuario gestor
     function obtenerVuelos(){
-        
+        try {
+        $conexion = new PDO("mysql:host=" . $GLOBALS['servidor'] . ";dbname=" . $GLOBALS['baseDatos'], $GLOBALS['usuario'], $GLOBALS['pass']);
+        $sql = $conexion->prepare("SELECT * from vuelos;");
+        $sql->execute();
+        $arrayVuelos=[];
+        while ($fila = $sql->fetch(PDO::FETCH_ASSOC)) {
+            $arrayVuelos[]=$fila;
+        }
+        $conexion=null;
+        return $arrayVuelos;
+
+        } catch (PDOException $e) {
+           echo $e;
+
+        }
+        $conexion=null;
+
     }
 
     function VuelosInsertar(){
-        
+        try {
+           
+        } catch (PDOException $e) {
+         echo $e;
+        }
     }
 
     function borrarVuelo(){
@@ -43,7 +93,72 @@ $pass="developer";
     function obtenerUsuario(){
 
     }
+consultaVuelosInvitadosLlegada("Madrid");
+ consultaVuelosInvitadosSalida("Sevilla");
+ 
+?>
 
+<!-- <thead>
+           <tr>
+           <th>Id</th>
+               <th>Origen</th>
+               <th>Destino</th>
+               <th>Operadora</th>
+               <th>Fecha</th>
+               <th>CantidadViajeros</th>
+              
+           </tr>
+       </thead>
+       <tbody> -->
+<?php
+       $lista=obtenerVuelos();
 
-   
+       echo "<table border=1px>";
+foreach($lista as $fila) {
+  
+  
+
+     echo "<tr>";
+     echo "<td>";
+  
+     echo $fila['id'];
+  
+     echo "</td>";
+    
+     echo "<td>";
+  
+     echo $fila['origen'];
+  
+     echo "</td>";
+  
+     echo "<td>";
+  
+     echo $fila['destino'];
+  
+     echo "</td>";
+  
+     echo "<td>";
+  
+     echo $fila['operadora'];
+  
+     echo "</td>";
+  
+     echo "<td>";
+  
+     echo $fila['fecha'];
+  
+     echo "</td>";
+  
+     echo "<td>";
+  
+     echo $fila['cantidadViajero'];
+  
+     echo "</td>";
+
+    
+     echo "</tr>";
+    
+   }
+   echo "</table>";
+   echo "<br>";
 ?>
